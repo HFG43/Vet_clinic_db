7b2552a3dc4076a2a0538e9bc419030540c1daf7
HFG43 <hernanguemes@hotmail.com>
2023-08-15 19:29:18 -0300

    feat: create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
-);
+    id INT PRIMARY KEY,
+    name VARCHAR (25),
+    date_of_birth DATE,
+    escape_attemps INT,
+    neutered BOOLEAN,
+    weigth_kg DECIMAL(3,2)
+    );
+
