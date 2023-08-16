121cc8bc6cbd2d4baf2a9b225d34b2a7e0ded705
HFG43 <hernanguemes@hotmail.com>
2023-08-16 00:56:15 -0300

    feat: correct date_of_birth query

--- queries.sql
+++ queries.sql
@@ -3,9 +3,8 @@
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-12-31';
 SELECT name FROM animals WHERE neutered = true AND escape_attemps < 3;
-SELECT date_of_birth FROM animals WHERE name NOT IN ('Agumon','Pikachu');
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attemps FROM animals WHERE weigth_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weigth_kg >= 10.4 AND weigth_kg <= 17.3;
-SELECT weigth_kg FROM animals WHERE name IN ('Agumon','Pikachu');
