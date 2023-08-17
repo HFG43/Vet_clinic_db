66958b00232b4a37c76c4e3ce59290e084448cfb
HFG43 <hernanguemes@hotmail.com>
2023-08-16 21:10:09 -0300

    Feat: inside transactions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,24 @@ SELECT name, escape_attemps FROM animals WHERE weigth_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weigth_kg >= 10.4 AND weigth_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT save1;
+UPDATE animals SET weigth_kg = weigth_kg * -1;
+ROLLBACK TO SAVEPOINT save1;
+UPDATE animals SET weigth_kg = weigth_kg * -1 WHERE weigth_kg < 0;
+COMMIT;
